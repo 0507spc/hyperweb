ecffaeb77d5bc7c6c99088f89f9a05ddf20412ce
0507spc <0507spc@gmail.com>
2021-11-21 23:15:15 +0000

    Update RealDebrid.js

--- RealDebrid.js
+++ RealDebrid.js
@@ -5,40 +5,47 @@
 //       https://api.real-debrid.com/rest/1.0/hosts/regex
 // ==/UserScript==
 'use strict';
+function genShortcutLinks() {
+    var regex = RegExp('\\b(' + [
+    'dropapk\\.to/',
+    'https...(rapidgator\\.net|rg\\.to)/file',
+    'mega\\.co\\.nz/#!',
+    'userscloud\\.com/[a-z0-9]+',
+    'nitroflare\\.com/view',
+    'oboom\\.com/.+\\.',
+    'sendspace\\.com/file',
+    'share-online\\.biz/dl/',
+    'turbobit\\.net/.+html',
+    'tusfiles\\.net/[a-z0-9]+$',
+    'ul\\.to/',
+    'uploaded\\.(to|net)/file',
+    'uploadrocket.net/[a-z0-9]+',
+    'uptobox\\.com/[a-z0-9]+',
+    'zippyshare\\.com/',
+    'http...audiobookbay\\.nl/download',
+    'magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+$/i : /magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+'
+    ].join('|') + ')', 'i');
 
-var regex = RegExp('\\b(' + [
-'dropapk\\.to/',
-'https...(rapidgator\\.net|rg\\.to)/file',
-'mega\\.co\\.nz/#!',
-'userscloud\\.com/[a-z0-9]+',
-'nitroflare\\.com/view',
-'oboom\\.com/.+\\.',
-'sendspace\\.com/file',
-'share-online\\.biz/dl/',
-'turbobit\\.net/.+html',
-'tusfiles\\.net/[a-z0-9]+$',
-'ul\\.to/',
-'uploaded\\.(to|net)/file',
-'uploadrocket.net/[a-z0-9]+',
-'uptobox\\.com/[a-z0-9]+',
-'zippyshare\\.com/',
-'http...audiobookbay\\.nl/download',
-'magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+$/i : /magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+'
-].join('|') + ')', 'i');
+    var prepend = "shortcuts\://run-shortcut?name=RealDebridDynamic&input=" ;
 
-var prepend = "shortcuts\://run-shortcut?name=RealDebridDynamic&input=" ;
+    var links,thisLink;
+    links = document.evaluate("//a[@href]",
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);
 
-var links,thisLink;
-links = document.evaluate("//a[@href]",
-    document,
-    null,
-    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-    null);
+    for (var i=0;i<links.snapshotLength;i++) {
+        var thisLink = links.snapshotItem(i);
+        thisLink.href = thisLink.href.replace(regex,prepend + '$1');
+    }
 
-for (var i=0;i<links.snapshotLength;i++) {
-    var thisLink = links.snapshotItem(i);
+}
 
-  thisLink.href = thisLink.href.replace(regex,
-                                      prepend + '$1');
+genShortcutLinks() ;
 
+var els = document.getElementsByTagName("*");
+for(var i = 0, l = els.length; i < l; i++) {
+  var el = els[i];
+  el.innerHTML = el.innerHTML.replace(re, 'javascript:void(0)' + 'javascript:void(0) ; genShortcutLinks();' );
 }
